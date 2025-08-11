020fd4630ca63f4cca69c47c374aa7b549afea30
Soham Deshmukh <soham.deshmukh.college@gmail.com>
2025-08-11 18:01:12 -0400

    Update ContainsDuplicate.py

--- Arrays & Hashing/ContainsDuplicate.py
+++ Arrays & Hashing/ContainsDuplicate.py
@@ -1,1 +1,16 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        #Hashmap
+        intMap = {} #{value:count}
 
+    
+        for n in nums:
+            if not n in intMap: 
+                intMap[n] = 1
+            else: #duplicate is found
+                return True 
+        return False
