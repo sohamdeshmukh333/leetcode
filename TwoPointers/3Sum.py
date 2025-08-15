d8835c4a0c4a9d1bf1343285bd1d5bda26e5c91b
Soham Deshmukh <soham.deshmukh.college@gmail.com>
2025-08-15 14:24:40 -0400

    August 15th, 2025

---
+++ TwoPointers/3Sum.py
@@ -0,0 +1,29 @@
+# August 15, 2025
+# Sort the array, then for each element use two pointers to find pairs that sum to its negative. 
+# Skip duplicates to avoid repeated triplets.
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        nums.sort()
+    
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i - 1]:
+                continue
+        
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+        return res
